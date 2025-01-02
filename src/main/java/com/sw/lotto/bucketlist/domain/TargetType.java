91a0a880ee72c0d154ad5eb2db4bb63402326f24
Kim-2301 <seunghhhh2344@gmail.com>
2025-01-02 22:11:38 +0900

    [fix]

--- src/main/java/com/sw/lotto/bucketlist/domain/TargetType.java
+++
@@ -1,7 +0,0 @@
-package com.sw.lotto.bucketlist.domain;
-
-public enum TargetType {
-    CAR,
-    REAL_ESTATE,
-    LUXURY
-}
--- src/main/java/com/sw/lotto/bucketlist/repository/CartItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sw.lotto.bucketlist.repository;
-
-import com.sw.lotto.bucketlist.domain.CartItemEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CartItemRepository extends JpaRepository<CartItemEntity, Long> {
-}
