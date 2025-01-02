1411fac1be022d8600398e93060a4ab31010c256
Kim-2301 <seunghhhh2344@gmail.com>
2025-01-02 19:42:14 +0900

    [fix] lotto 조회 시 인증 필요없게 수정

--- src/main/java/com/sw/lotto/global/config/SwaggerConfig.java
+++ src/main/java/com/sw/lotto/global/config/SwaggerConfig.java
@@ -1,12 +1,20 @@
 package com.sw.lotto.global.config;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+
 import io.swagger.v3.oas.models.OpenAPI;
 
-
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,  // HTTP 타입 사용
+        scheme = "bearer",
+        bearerFormat = "JWT"  // JWT 사용
+)
 @Configuration
 public class SwaggerConfig {
 
