c93d0803e97d37c0b83a1d2b238814b7d56596d1
Kim-2301 <seunghhhh2344@gmail.com>
2025-02-12 22:16:06 +0900

    [fix] 테스트 어노테이션 설정 추가

--- src/test/java/com/sw/lotto/LottoApplicationTests.java
+++ src/test/java/com/sw/lotto/LottoApplicationTests.java
@@ -3,7 +3,7 @@ package com.sw.lotto;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes= LottoApplicationTests.class)
 class LottoApplicationTests {
 
     @Test
